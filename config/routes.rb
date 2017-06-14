39d7d89119c582d5db988f0170217bfb6c7e9d40
sagae keiga <sagae5.28rujeae@gmail.com>
2017-06-14 12:20:14 +0000

    search

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   
   namespace :api, { format: 'json' } do
     namespace :v1, { format: 'json' } do 
-      post 'images/search'
+      get 'images/search'
 
       resources :images, :only => [:index]
       resources :movies
