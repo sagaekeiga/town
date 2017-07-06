175ec4bbeb905c190cb54faeb89c7cee32aaa3e4
sagae keiga <sagae5.28rujeae@gmail.com>
2017-07-06 13:51:58 +0000

    g

--- db/migrate/20170522124052_create_images.rb
+++ db/migrate/20170522124052_create_images.rb
@@ -1,20 +1,19 @@
 class CreateImages < ActiveRecord::Migration[5.0]
   def change
     create_table :images do |t|
-      t.text :en_title
-      t.text :ja_title
-      t.text :en_content
-      t.text :ja_content
-      t.text :location
-      t.text :url
-      t.text :image
-      t.text :start
-      t.text :key
-      t.text :movie
+      t.text :en_title, :limit => 100
+      t.text :ja_title, :limit => 100
+      t.text :en_content, :limit => 100
+      t.text :ja_content, :limit => 100
+      t.text :location, :limit => 100
+      t.text :url, :limit => 100
+      t.text :image, :limit => 100
+      t.text :start, :limit => 100
+      t.text :key, :limit => 100
+      t.text :movie, :limit => 100
       
 
       t.timestamps
     end
-    add_index :images, [:en_title, :ja_title]
   end
 end
--- db/migrate/20170604100110_create_movies.rb
+++ db/migrate/20170604100110_create_movies.rb
@@ -1,15 +1,15 @@
 class CreateMovies < ActiveRecord::Migration[5.0]
   def change
     create_table :movies do |t|
-      t.text :en_title
-      t.text :ja_title
-      t.text :en_content
-      t.text :ja_content
-      t.text :present
-      t.text :destination
-      t.text :url
-      t.text :key
-      t.text :movie
+      t.text :en_title, :limit => 100
+      t.text :ja_title, :limit => 100
+      t.text :en_content, :limit => 100
+      t.text :ja_content, :limit => 100
+      t.text :present, :limit => 100
+      t.text :destination, :limit => 100
+      t.text :url, :limit => 100
+      t.text :key, :limit => 100
+      t.text :movie, :limit => 100
 
       t.timestamps
     end
