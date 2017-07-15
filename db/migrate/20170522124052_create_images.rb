e2c40c6211f6ef5e4368debaa204719924b57db4
sagae keiga <sagae5.28rujeae@gmail.com>
2017-07-15 01:59:47 +0000

    g

--- db/migrate/20170522124052_create_images.rb
+++ db/migrate/20170522124052_create_images.rb
@@ -1,16 +1,16 @@
 class CreateImages < ActiveRecord::Migration[5.0]
   def change
     create_table :images do |t|
-      t.text :en_title, :limit => 100
-      t.text :ja_title, :limit => 100
-      t.text :en_content, :limit => 100
-      t.text :ja_content, :limit => 100
-      t.text :location, :limit => 100
-      t.text :url, :limit => 100
-      t.text :image, :limit => 100
-      t.text :start, :limit => 100
-      t.text :key, :limit => 100
-      t.text :movie, :limit => 100
+      t.text :en_title, :limit => 1000
+      t.text :ja_title, :limit => 1000
+      t.text :en_content, :limit => 1000
+      t.text :ja_content, :limit => 1000
+      t.text :location, :limit => 1000
+      t.text :url, :limit => 1000
+      t.text :image, :limit => 1000
+      t.text :start, :limit => 1000
+      t.text :key, :limit => 1000
+      t.text :movie, :limit => 1000
       
 
       t.timestamps
